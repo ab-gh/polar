bd4b2ede925316b32b82dade04bfdccf0f22703d
ash#0001 <thomasbass2001@gmail.com>
2018-10-27 17:03:36 +0100

    Update polar.py

--- polar.py
+++ polar.py
@@ -2,6 +2,7 @@
 import time
 from bh1745 import BH1745
 import dot3k.lcd as lcd
+import dot3k.backlight as backlight
 
 
 print("""
@@ -14,7 +15,7 @@ bh1745 = BH1745()
 
 bh1745.setup()
 bh1745.set_leds(0)
-
+backlight.rgb(200,200,200)
 time.sleep(1.0)  # Skip the reading that happened before the LEDs were enabled
 
 try:
