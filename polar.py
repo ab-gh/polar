f9f395681486aeb279cca9270e34853a4df67763
ash#0001 <thomasbass2001@gmail.com>
2018-10-27 20:51:24 +0100

    Update polar.py

--- polar.py
+++ polar.py
@@ -4,19 +4,11 @@ from bh1745 import BH1745
 import dot3k.lcd as lcd
 import dot3k.backlight as backlight
 
-
-print("""
-This example shows a basic "Hello World" on the LCD.
-You should see "Hello World" displayed on your LCD!
-Press CTRL+C to exit.
-""")
-
 bh1745 = BH1745()
-
 bh1745.setup()
 bh1745.set_leds(0)
 backlight.rgb(200,200,200)
-time.sleep(1.0)  # Skip the reading that happened before the LEDs were enabled
+time.sleep(1.0)
 
 try:
     while True:
@@ -26,6 +18,6 @@ try:
         lcd.clear()
         lcd.write('{:10.1f}'.format(c))
         time.sleep(0.1)
-
+        
 except KeyboardInterrupt:
     bh1745.set_leds(0)
